d07d9ae9352d6019f9758de946f5e7977af88748
Drew Ogle <drew@aperturedata.io>
2025-08-28 10:50:04 -0400

    fix scripts, pass in json init data for storages - it is complaining about the redirection inside them. remove it.

--- deploy/docker-entrypoint.d/app/31-run-label-studio-init.sh
+++ deploy/docker-entrypoint.d/app/31-run-label-studio-init.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e ${DEBUG:+-x}
+exec 3>&1
 
 echo >&3 "=> Run label-studio init..."
-label-studio init -q ${LABEL_STUDIO_CREATE_PROJ_TITLE} >&3
+if [ "${LABEL_STUDIO_CREATE_PROJ_TITLE}" != "" ]; then
+    echo >&3 "=> Creating Project \"${LABEL_STUDIO_CREATE_PROJ_TITLE}\""
+    label-studio init -q "${LABEL_STUDIO_CREATE_PROJ_TITLE}" >&3
+else
+    label-studio init -q >&3
+fi
 echo >&3 "=> label-studio init completed."
--- deploy/docker-entrypoint.d/app/32-run-label-studio-add-cloud-import.sh
+++ deploy/docker-entrypoint.d/app/32-run-label-studio-add-cloud-import.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e ${DEBUG:+-x}
+exec 3>&1
 
-echo >&3 "=> Run label-studio init..."
-label-studio storage aperturedb "Cloud ApertureDB"
-echo >&3 "=> label-studio init completed."
+if [ "${LABEL_STUDIO_DEFAULT_CLOUD_STORAGE}" != "" ]; then
+    JSON_ARG=""
+    JSON_CMD=""
+    if [ "${LABEL_STUDIO_CLOUD_STORAGE_JSON_PATH}" != "" ]; then
+        JSON_CMD="--config-json"
+        JSON_ARG="${LABEL_STUDIO_CLOUD_STORAGE_JSON_PATH}"
+    fi
+    echo >&3 "=> Run manage.py storage_mgr --action add"
+    cd label_studio && python3 manage.py storage_mgr --project 1 --action add --storage ${LABEL_STUDIO_DEFAULT_CLOUD_STORAGE} $JSON_CMD "$JSON_ARG"
+    echo >&3 "=> manage.py storage_mgr --action add completed."
+fi
